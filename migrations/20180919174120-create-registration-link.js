a2990c8a58ad8bc8b88afa4f5b5c9af6b4a261f9
Vigneshwaran <vikkiwaranj@gmail.com>
2018-10-06 15:46:09 +0530

    verify registration
    verify registration

---
+++ migrations/20180919174120-create-registration-link.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('RegistrationLinks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserID: {
+        type: Sequelize.STRING
+      },
+      LinkID: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('RegistrationLinks');
+  }
+};
